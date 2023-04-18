679dddc43fd53ae9f6a6eebb7bb5079c71e6d8e5
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-04-19 02:33:57 +0530

    Add files via upload

---
+++ Password vadildator/script.js
@@ -0,0 +1,10 @@
+function validateForm() {
+  const pass1 = document.getElementById("password1").value;
+  const pass2 = document.getElementById("password2").value;
+
+  if (pass1 != pass2) {
+    alert("Password didn't match. Password validation unsuccessful");
+  } else {
+    alert("Password matched.Password Validation Successful");
+  }
+}
