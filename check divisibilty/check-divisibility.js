79c40b66470bb0cf9a172650260946a075faafab
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-09 21:54:57 +0530

    Update check-divisibility.js

--- check divisibilty/check-divisibility.js
+++ check divisibilty/check-divisibility.js
@@ -6,10 +6,13 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 for (let i = 0; i < arr.length; i++) {
+  
   if (arr[i] % 2 == 0) {
     continue;
   }
+  
   if (arr[i] % 3 == 0) {
     console.log(arr[i]);
   }
+  
 }
