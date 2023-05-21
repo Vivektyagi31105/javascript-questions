7112607c80e410bd53f9365d62096cc35e81ead7
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-21 23:43:32 +0530

    Add files via upload

---
+++ Count Vowel/script.js
@@ -0,0 +1,16 @@
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+function countVowels(str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+const str = prompt("Type the name: ");
+const result = countVowels(str);
+
+console.log(`Total number of vowels in string is: ` + result);
