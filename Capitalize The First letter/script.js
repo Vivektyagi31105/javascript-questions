2d7fb566675614b5cc3092820d90ec869341ba9b
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-22 23:40:02 +0530

    Update script.js

--- Capitalize The First letter/script.js
+++ Capitalize The First letter/script.js
@@ -1,3 +1,8 @@
+// You are building a form where users can enter their names. You want to make sure that the first letter of the
+// name is always capitalized, even if the user forgets to do so. Write a program that takes in the user's name as a
+// string and uses the ternary operator to check if the first letter is lowercase. If it is, the program capitalizes it and
+// returns the modified string. Otherwise, it returns the original string without any changes.
+
 function capitalize(str) {
   let first_letter = str.charAt(0).toUpperCase();
   let capitalize_string = first_letter + str.slice(1);
