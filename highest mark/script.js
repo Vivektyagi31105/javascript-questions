cd3841d1659b2b3e3ae9e72e5d5815776f76447b
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-22 23:41:19 +0530

    Update script.js

--- highest mark/script.js
+++ highest mark/script.js
@@ -1,3 +1,9 @@
+// A teacher wants to find out the highest marks scored #y a student in a class of five students. The teacher enters
+// the marks of all five students in an array called "marks". Write a program that iterates through the array and
+// finds the highest marks scored by any student in the class. The highest marks must then be displayed to the
+// teacher using the console. Make sure you use the ternary operator to find the student with the highest marks.
+
+
 let marks = [40, 70, 78, 81, 69, 56, 60];
 let max = marks[0];
 
