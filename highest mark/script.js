5bd74d1e01d8925f4fbd995a56ca632610fe7016
Vivek Tyagi <105948234+Vivektyagi31105@users.noreply.github.com>
2023-05-24 23:40:36 +0530

    Update script.js

--- highest mark/script.js
+++ highest mark/script.js
@@ -8,10 +8,11 @@ let marks = [40, 70, 78, 81, 69, 56, 60];
 let max = marks[0];
 
 for (let i = 0; i < marks.length; i++) {
-  // if (marks[i] > max) {
-  //   max = marks[i];
-  // }
+ 
 
   max = marks[i] > max ? marks[i] : max;
+   // if (marks[i] > max) {
+  //   max = marks[i];
+  // }
 }
 console.log(`Highest mark is: ` + max + `.`);
